db026e1b9769d47cf917d8222205c69443cc81bd
meepobrother <1037483576@qq.com>
2019-01-07 17:52:15 +0800

    #0.0.7

--- publish/ims-wxapp/__tests__/ims-wxapp.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const imsWxapp = require('..');
-
-describe('ims-wxapp', () => {
-    it('needs tests');
-});
--- publish/ims-wxapp/lib/ims-wxapp.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = imsWxapp;
-
-function imsWxapp() {
-    // TODO
-}
