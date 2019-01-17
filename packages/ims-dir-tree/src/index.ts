252ab9c284abbc2410f4446f429b798040cd873c
meepobrother <1037483576@qq.com>
2019-01-17 16:31:02 +0800

    Update ims-dir-tree
    添加接口

--- packages/ims-dir-tree/src/index.ts
+++ packages/ims-dir-tree/src/index.ts
@@ -41,6 +41,59 @@ export class ImsDirTree {
       await forkJoin(...obs).toPromise();
     }
   }
+
+  /**
+   * 新建文件夹
+   * @param name
+   * @param options
+   */
+  async mkdir(
+    name: string,
+    options?: number | string | fs.MakeDirectoryOptions | null,
+  ): Promise<void> {
+    return new Promise((resolve, reject) => {
+      fs.mkdir(path.join(this.base, name), options, (err: any) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    });
+  }
+
+  async exists(name: string): Promise<Boolean> {
+    return new Promise((resolve, reject) => {
+      return fs.exists(path.join(this.base, name), (exsit: boolean) => {
+        resolve(exsit);
+      });
+    });
+  }
+
+  async existsDir(name: string): Promise<Boolean> {
+    let exist = await this.exists(name);
+    if (exist) {
+      return true;
+    }
+    return await this.exists(path.dirname(path.join(this.base, name)));
+  }
+
+  /**
+   * 写入文件
+   * @param name
+   * @param buf
+   */
+  async writeFile(name: string, buf: Buffer) {
+    if (await this.existsDir(name)) {
+      await writeFile(path.join(this.base, name), buf);
+    }
+  }
+}
+
+function writeFile(path: string, buf: Buffer) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, buf, err => {
+      if (err) return reject(err);
+      resolve();
+    });
+  });
 }
 
 /**
